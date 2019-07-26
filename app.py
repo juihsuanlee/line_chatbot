9b76cef5aa5073413830dbec4b3e211feade0102
juihsuanlee <emomejui@gmail.com>
2019-07-26 13:16:59 +0800

    Add code

--- app.py
+++ app.py
@@ -33,9 +33,11 @@ def callback():
 # 處理訊息
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
-
-    message = TextSendMessage(text=event.message.text.encode('utf-8'))
-    print(message)
+	temp = event.message.text
+	message_encode = temp.encode('utf-8')
+	print(message_encode)
+	
+    message = TextSendMessage(text=event.message.text)
     line_bot_api.reply_message(event.reply_token, message)
 
 import os
